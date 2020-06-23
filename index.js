c8d8e71919770ec60cd267010e94745d535763fc
Lucia <lumiraelmar@gmail.com>
2020-06-23 16:05:44 +0200

    cruces

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-fetch('https://gomakethings.com/getting-html-with-fetch-in-vanilla-js/').then(function (response) {
-	// The API call was successful!
-	return response.text();
-}).then(function (html) {
-
-	// Convert the HTML string into a document object
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(html, 'text/html');
-
-}).catch(function (err) {
-	// There was an error
-	console.warn('Something went wrong.', err);
-});
+const cruz = document.querySelectorAll(".fa");

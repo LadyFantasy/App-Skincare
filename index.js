01e8ec1d7a643ecad411296baa213629b6c0bea6
LadyFantasy <msoledadm88@gmail.com>
2020-06-25 20:33:16 +0100

    inputs

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
+const icons = document.querySelectorAll(".icon")
+const mascarillaWrapper = document.querySelector(".mascarillaWrapper")
+
+const routineInputs = document.querySelector(".routineInputs")
+
+document.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("icon")) {
+    return
+  }
+
+  addInput(e)
+
+})
+
+
+function addInput(e) {
+  const inputWapper = document.createElement("div")
+  const input = document.createElement("input")
+  input.type = "text"
+  input.placeholder = e.target.nextElementSibling.innerHTML
+  const iconClose = document.createElement("i")
+
+  routineInputs.appendChild(inputWapper)
+  routineInputs.classList.add("routineInputs")
+  inputWapper.appendChild(input)
+  inputWapper.appendChild(iconClose)
+  iconClose.classList.add("fa", "fa-times")
+  inputWapper.classList.add("inputWrapper")
+  input.classList.add("input")
+  
+
+  // falta agregar la cruz de borrar
+}
 
 
 
 document.addEventListener("click", (event) => {
-  if (event.target.closest(".fa.fa-times")) {
-      
-console.log (event.target.parentNode);
-  event.target.parentNode.parentNode.remove()
+  if (!event.target.closest(".fa.fa-times")) {
+    return
   }
+
+  console.log(event.target.parentNode);
+  event.target.parentNode.remove()
 })
